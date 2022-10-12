aaaccb885804fc4a35466900dab33447ce4d1304
Gustavo Junkes <gujunkes50@gmail.com>
2022-10-11 21:06:14 -0300

    Extracting class to its own file/module.

--- src/app.ts
+++ src/app.ts
@@ -2,51 +2,7 @@ import express, {Request, Response} from "express"
 import { sensitiveHeaders } from "http2";
 import { start } from 'repl';
 import { create, Whatsapp } from 'venom-bot';
-
-
-// todo: extrair classe para outro arquivo
-class MessageSender {
-    client!: Whatsapp;
-    statusSession!: string;
-    qrCode!: string
-
-    constructor() {
-        this.init();
-    }
-
-    private init() {
-        const venom = require('venom-bot');
-
-        const start = (client: Whatsapp) => {
-            this.client = client;
-          //  this.sendText("554788591737", "Olá"); // todo: receber numero e mensagem por parametro
-        }
-
-        const qr = (base64QrImg: string) => {
-            this.qrCode = base64QrImg;
-        }
-
-        const status = (statusSession: string) => {
-            this.statusSession = statusSession;
-        }
-
-        venom.create({
-            session: 'main-session'
-        }, qr )
-        .then((client: Whatsapp) => start(client))
-        .catch((error: string) => {
-            console.error(error)
-        })
-    }   
-
-    async sendText(to: string, text: string) {
-        if(!to.includes("@c.us")) {
-            to = to + "@c.us";
-        }
-        this.client.sendText(to, text);
-    }
-}
-
+import { MessageSender } from "./messageSender";
 
 const messageSender = new MessageSender();
 
@@ -57,11 +13,13 @@ app.listen(8080);
 
 // todo: implementar chamada api get para status da conexão (retorna o qr code se estiver desconectado)
 app.get("/connectionStatus", (request: Request, response: Response) => {
+        messageSender.getConnectionState().then((state) => {
+           return response.status(200).json(state)
+        });
     
 })
-// todo: implementar chamada api para enviar mensagem
 
- app.post("/sendMessage", async (request: Request, response: Response) => {
+app.post("/sendMessage", async (request: Request, response: Response) => {
     const {phone, message}  = request.body;
     
     try {
@@ -72,3 +30,4 @@ app.get("/connectionStatus", (request: Request, response: Response) => {
     } 
     return response.status(200).json({message: "The given message was successfully sent."}); 
 })
+

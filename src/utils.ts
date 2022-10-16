bac5e37634e6bf94231b66548681ca8322380ea7
Gustavo Junkes <gujunkes50@gmail.com>
2022-10-16 16:20:28 -0300

    Create method to compare strings

--- src/utils.ts
+++ src/utils.ts
@@ -1,2 +1,8 @@
 export class Utils {
+
+    valueInIgnoreCase (str1: string, str2: string): boolean {
+        var toReturn = false;
+
+        str1.localeCompare(str2, undefined, {sensitivity: 'accent'})
+    }
 }

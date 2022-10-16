a6c4aa356e410e96dfa10698576e7a6707069962
Gustavo Junkes <gujunkes50@gmail.com>
2022-10-16 16:39:29 -0300

    Implementing aux method to compare strings.

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,21 @@
 export class Utils {
 
     valueInIgnoreCase (str1: string, str2: string): boolean {
-        var toReturn = false;
+        if (str1 === str2) {
+            return true;
+        }    
+        // colocar uppercase e verificar se 2 esta em 1
+        str1 = str1.toLocaleLowerCase();
+        str2 = str2.toLocaleLowerCase();
 
-        str1.localeCompare(str2, undefined, {sensitivity: 'accent'})
+        if(str1 == str2) {
+            return true
+        }
+
+        if (str1.includes(str2)) {
+            return true;
+        }
+        return false;
     }
 }
 global.exports.utils = Utils
